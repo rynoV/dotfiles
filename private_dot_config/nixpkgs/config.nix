fb56b321e36445b48da7f1344b70d3ac8b7f80a0
Calum Sieppert <sieppertcalum@gmail.com>
2022-04-13 16:49:35 -0600

    gh, ghci, nix, mimeapps

---
+++ private_dot_config/nixpkgs/config.nix
@@ -0,0 +1,16 @@
+# https://nixos.org/manual/nixpkgs/stable/#sec-declarative-package-management
+{
+  packageOverrides = pkgs: with pkgs; rec {
+    myPackages = pkgs.buildEnv {
+      name = "my-packages";
+      paths = [
+        # Allows configuration of nix-shell to use fish
+        any-nix-shell
+        ripgrep
+        ripgrep-all
+      ];
+      pathsToLink = [ "/share/man" "/share/doc" "/bin" "/etc" ];
+      extraOutputsToInstall = [ "man" "doc" ];
+    };
+  };
+}
