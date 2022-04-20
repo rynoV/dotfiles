9e1e82899c9f4014b6f5258f27266e9dc9bc8071
Calum Sieppert <sieppertcalum@gmail.com>
2022-04-19 22:18:40 -0600

    Open web links with ctrl click in emacs

--- private_dot_config/emacs/init.el
+++ private_dot_config/emacs/init.el
@@ -27,6 +27,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function 'browse-url-default-browser)
  '(lsp-ui-sideline-delay 0.5)
  '(lsp-ui-sideline-show-hover t)
  '(lsp-ui-sideline-update-mode nil)
