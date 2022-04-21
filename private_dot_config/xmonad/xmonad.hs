fb0f4a28e9b282698739df05025539f4a2f15e67
Calum Sieppert <sieppertcalum@gmail.com>
2022-04-21 14:54:37 -0600

    Initial xmonad config

---
+++ private_dot_config/xmonad/xmonad.hs
@@ -0,0 +1,16 @@
+import XMonad
+import XMonad.Config.Desktop
+import XMonad.Util.SpawnOnce
+
+main = xmonad def
+    { terminal = "kitty",
+      modMask = mod4Mask,
+      focusedBorderColor = "#88C0D0",
+      normalBorderColor = "#2E3440",
+      focusFollowsMouse = False,
+      startupHook = myStartupHook
+    }
+
+myStartupHook :: X ()
+myStartupHook = do
+    spawnOnce "picom --no-vsync"
