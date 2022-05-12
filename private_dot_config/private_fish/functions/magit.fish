6ffb67119b645c6afd54e84c0694d3518ddab6f1
Calum Sieppert <sieppertcalum@gmail.com>
2022-05-12 12:24:03 -0600

    Add option to magit function to open using existing daemon

--- private_dot_config/private_fish/functions/magit.fish
+++ private_dot_config/private_fish/functions/magit.fish
@@ -1,4 +1,5 @@
-function magit --description 'Start Magit'
+function magit --description 'Start Magit. Use -q/--quick to start using emacsclient'
+    argparse --stop-nonopt --name=magit 'q/quick' -- $argv; or return
     # https://stackoverflow.com/a/29635131/14703577
     # Check if argument was provided and that it is a directory
     if begin; test (count $argv) -gt 0; and test -d $argv[1]; end
@@ -7,5 +8,9 @@ function magit --description 'Start Magit'
         set dir $PWD
     end
     # Note that the & makes this run in the background
-    emacs --magit --eval "(magit-status)" -d :0 --chdir $dir &
+    if set --query _flag_quick
+        myemacs --no-wait --eval "(magit-status \"$dir\")"
+    else
+        emacs --magit --eval "(magit-status)" --chdir $dir &
+    end
 end
