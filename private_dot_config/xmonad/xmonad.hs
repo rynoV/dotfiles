a51adce6f683d67e11b0f5a6410c75c6041d99cc
Calum Sieppert <sieppertcalum@gmail.com>
2022-05-14 22:50:22 -0600

    Htop and formatting

--- private_dot_config/xmonad/xmonad.hs
+++ private_dot_config/xmonad/xmonad.hs
@@ -15,6 +15,7 @@ import qualified Data.Map as M
 import Data.Maybe (fromMaybe, isJust)
 import GHC.Generics (Generic)
 import System.Environment (getEnv)
+import System.Exit (exitSuccess)
 import System.IO (hClose, openFile)
 import System.Posix (executeFile, mkstemp)
 import XMonad
@@ -121,7 +122,6 @@ import XMonad.Util.Run (hPutStr, runProcessWithInput)
 import XMonad.Util.SpawnOnce (spawnOnce)
 import XMonad.Util.Ungrab (unGrab)
 import XMonad.Util.WorkspaceCompare (filterOutWs)
-import System.Exit (exitSuccess)
 
 main :: IO ()
 main =
@@ -189,15 +189,16 @@ myScratchpads =
       $ customFloating $ W.RationalRect (1 / 6) (1 / 6) (2 / 3) (2 / 3)
   ]
 
+addMyKeymap c = (subtitle "Custom Keys" :) $ mkNamedKeymap c myKeymap
+
 -- Note: M1 is left alt, C-S-M1-M is the "hyper" key and C-S-M1 is the
 -- "meh" key (special keys on my keyboard that emulate these
 -- modifiers)
-addMyKeymap c = (subtitle "Custom Keys" :) $ mkNamedKeymap c myKeymap
-
 myKeymap =
   [ ("M-<Tab>", addName "Previous window" $ nextMatch History (return True)),
     ("M1-<Tab>", addName "Cycle windows in MRU order" $ cycleRecentWindows [xK_Alt_L] xK_Tab xK_q),
     ("M-<Esc>", addName "Suspend" $ spawn "systemctl suspend"),
+    ("C-S-M1-r", addName "Clipboard history" $ spawn "copyq toggle"),
     -- Move focus to prev/next screen
     ("M-a", addName "Focus previous screen" $ onPrevNeighbour def W.view),
     ("M-o", addName "Focus previous screen" $ onNextNeighbour def W.view),
@@ -205,7 +206,10 @@ myKeymap =
     ("M-S-a", addName "Move window to previous screen" $ onPrevNeighbour def W.shift),
     ("M-S-o", addName "Move window to next screen" $ onNextNeighbour def W.shift),
     -- Click a window or drag a rectangle to screenshot it
-    ("M-s s", addName "Screenshot window or rectangle" $ unGrab *> spawn "scrot --freeze --line width=2,color=\"red\",mode=\"edge\" --select"),
+    ( "M-s s",
+      addName "Screenshot window or rectangle" $
+        unGrab *> spawn "scrot --freeze --line width=2,color=\"red\",mode=\"edge\" --select"
+    ),
     -- Take a screenshot of all monitors
     ("M-s d", addName "Screenshot all monitors" $ unGrab *> spawn "scrot"),
     -- Take a screenshot of the current window
@@ -222,8 +226,9 @@ myKeymap =
     ("M-C-<Down>", addName "Swap with downward window" $ sendMessage $ Swap D),
     -- Prompts
     ( "M-p w",
-      addName "Select a window in the current workspace, or hit ` and bring a window from another workspace" $
-        windowMultiPrompt
+      addName
+        "Select a window in the current workspace, or hit ` and bring a window from another workspace"
+        $ windowMultiPrompt
           myPromptConfig {autoComplete = Just 500000}
           [ (BringToMaster, wsWindows),
             ( Bring, -- Select among all windows not in the current workspace
@@ -238,8 +243,14 @@ myKeymap =
     ("M-p m", addName "Select a man page" $ manPrompt myPromptConfig),
     ("M-p s", addName "Run a shell command" $ shellPrompt myPromptConfig),
     ("M-p x", addName "Run an xmonad command" $ xmonadPrompt myPromptConfig),
-    ("M-p p f", addName "Select then type a username and password from bitwarden" $ bwLoginFillPrompt myPromptConfig),
-    ("M-p p c", addName "Select then copy a username and password from bitwarden" $ bwLoginCopyPrompt myPromptConfig),
+    ( "M-p p f",
+      addName "Select then type a username and password from bitwarden" $
+        bwLoginFillPrompt myPromptConfig
+    ),
+    ( "M-p p c",
+      addName "Select then copy a username and password from bitwarden" $
+        bwLoginCopyPrompt myPromptConfig
+    ),
     -- App launchers
     ("C-S-M1-e", addName "Launch emacs" $ spawn "myemacs --create-frame"),
     ("C-S-M1-b", addName "New browser window" $ spawn "browser"),
@@ -248,11 +259,17 @@ myKeymap =
     -- Emacs launchers (mnemonic "E-macs")
     ("C-S-M1-M-e n", addName "Capture a note" $ spawn "~/scripts/org-capture n"),
     ("C-S-M1-M-e t", addName "Capture a todo item" $ spawn "~/scripts/org-capture t"),
-    ("C-S-M1-M-e c", addName "Run a quick calculation and copy to clipboard" $ spawn "~/scripts/emacs-calc"),
+    ( "C-S-M1-M-e c",
+      addName "Run a quick calculation and copy to clipboard" $
+        spawn "~/scripts/emacs-calc"
+    ),
     -- Bluetooth (mnemonic "C-onnection")
     ("C-S-M1-M-c o", addName "Bluetooth on" $ spawn "bluetoothctl power on"),
     ("C-S-M1-M-c f", addName "Bluetooth off" $ spawn "bluetoothctl power off"),
-    ("C-S-M1-M-c m", addName "Connect airpods and turn on mic" $ spawn "~/scripts/connect-airpods.sh"),
+    ( "C-S-M1-M-c m",
+      addName "Connect airpods and turn on mic" $
+        spawn "~/scripts/connect-airpods.sh"
+    ),
     ("C-S-M1-M-c c", addName "Connect airpods" $ spawn "~/scripts/connect-airpods-no-mic.sh"),
     ("C-S-M1-M-c d", addName "Disconnect airpods" $ spawn "~/scripts/disconnect-airpods.sh"),
     -- Scratchpads
@@ -266,7 +283,10 @@ myKeymap =
     ("M-w e", addName "Go to an empty workspace" $ moveTo Next emptyWS),
     ("M-w p", addName "Select/create a workspace by name" $ selectWorkspace myPromptConfig),
     -- Select a workspace to shift the current window to
-    ("M-w s", addName "Move window to a workspace" $ withWorkspace myPromptConfig (windows . W.shift)),
+    ( "M-w s",
+      addName "Move window to a workspace" $
+        withWorkspace myPromptConfig (windows . W.shift)
+    ),
     ("M-w r", addName "Rename workspace" $ renameWorkspace myPromptConfig),
     -- Move to/move window to next/prev workspace
     ("M-w j", addName "Move to next workspace" nextWS),
@@ -276,19 +296,40 @@ myKeymap =
     ("M-w S-M-j", addName "Move window to next workspace" shiftToNext),
     ("M-w S-M-k", addName "Move window to previous workspace" shiftToPrev),
     -- Move all windows to the next screen
-    ("M-w a", addName "Move all windows to the next screen" $ withAll (\w -> focus w >> onNextNeighbour def W.shift)),
+    ( "M-w a",
+      addName "Move all windows to the next screen" $
+        withAll (\w -> focus w >> onNextNeighbour def W.shift)
+    ),
     -- Searching for things, either through the prompt or using the
     -- current selection (mnemonic "G-oogle")
-    ("M-g g", addName "Google search" $ promptSearchBrowser myPromptConfig "browser" (intelligent google)),
-    ("M-g m", addName "Search Arch man pages" $ promptSearchBrowser myPromptConfig {defaultText = "!archman "} "browser" duckduckgo),
-    ("M-g t", addName "Google the current selection" $ selectSearchBrowser "browser" (intelligent google)),
-    ("M-g d", addName "Lookup the current selection in the dictionary" $ selectSearchBrowser "browser" dictionary),
+    ( "M-g g",
+      addName "Google search" $
+        promptSearchBrowser myPromptConfig "browser" (intelligent google)
+    ),
+    ( "M-g m",
+      addName "Search Arch man pages" $
+        promptSearchBrowser myPromptConfig {defaultText = "!archman "} "browser" duckduckgo
+    ),
+    ( "M-g t",
+      addName "Google the current selection" $
+        selectSearchBrowser "browser" (intelligent google)
+    ),
+    ( "M-g d",
+      addName "Lookup the current selection in the dictionary" $
+        selectSearchBrowser "browser" dictionary
+    ),
     ("M-g h", addName "Hoogle the current selection" $ selectSearchBrowser "browser" hoogle),
     ("M-g f", addName "Hoogle search" $ promptSearchBrowser myPromptConfig "browser" hoogle),
     ("M-<F10>", addName "Reload the monitor layout" $ spawn "autorandr --force --change"),
     -- Adjust monitor brightness
-    ("M-<F11>", addName "Decrease monitor brightness" $ spawn "light -s sysfs/backlight/ddcci14 -U 1 && light -s sysfs/backlight/ddcci14 -O"),
-    ("M-<F12>", addName "Increase monitor brightness" $ spawn "light -s sysfs/backlight/ddcci14 -A 1 && light -s sysfs/backlight/ddcci14 -O")
+    ( "M-<F11>",
+      addName "Decrease monitor brightness" $
+        spawn "light -s sysfs/backlight/ddcci14 -U 1 && light -s sysfs/backlight/ddcci14 -O"
+    ),
+    ( "M-<F12>",
+      addName "Increase monitor brightness" $
+        spawn "light -s sysfs/backlight/ddcci14 -A 1 && light -s sysfs/backlight/ddcci14 -O"
+    )
   ]
 
 myStartupHook :: X ()
@@ -367,36 +408,29 @@ zenity title msg = void . xfork $ do
 
 -- | Custom name default keys
 myDefaultKeys :: XConfig Layout -> [((KeyMask, KeySym), NamedAction)]
-myDefaultKeys conf@XConfig{XMonad.modMask = modm} =
-    [ subtitle "launching and killing programs"
-    , ((modm .|. shiftMask, xK_c     ), addName "Close the focused window" kill) -- %! Close the focused window
-
-    , subtitle "changing layouts"
-    , ((modm,               xK_space ), sendMessage' NextLayout) -- %! Rotate through the available layout algorithms
-    , ((modm .|. shiftMask, xK_space ), addName "Reset the layout" $ setLayout $ XMonad.layoutHook conf) -- %!  Reset the layouts on the current workspace to default
-
-    , separator
-    , ((modm,               xK_n     ), addName "Refresh" refresh) -- %! Resize viewed windows to the correct size
-
-    , subtitle "move focus up or down the window stack"
-    , ((modm,               xK_j     ), addName "Focus down" $ windows W.focusDown) -- %! Move focus to the next window
-    , ((modm,               xK_k     ), addName "Focus up"   $ windows W.focusUp  ) -- %! Move focus to the previous window
-    , ((modm,               xK_m     ), addName "Focus the master" $ windows W.focusMaster  ) -- %! Move focus to the master window
-
-    , subtitle "modifying the window order"
-    , ((modm,               xK_Return), addName "Swap with the master" $ windows W.swapMaster) -- %! Swap the focused window and the master window
-    , ((modm .|. shiftMask, xK_j     ), addName "Swap down" $ windows W.swapDown  ) -- %! Swap the focused window with the next window
-    , ((modm .|. shiftMask, xK_k     ), addName "Swap up"   $ windows W.swapUp    ) -- %! Swap the focused window with the previous window
-
-    , subtitle "resizing the master/slave ratio"
-    , ((modm,               xK_h     ), sendMessage' Shrink) -- %! Shrink the master area
-    , ((modm,               xK_l     ), sendMessage' Expand) -- %! Expand the master area
-
-    , subtitle "change the number of windows in the master area"
-    , ((modm              , xK_comma ), sendMessage' (IncMasterN 1)) -- %! Increment the number of windows in the master area
-    , ((modm              , xK_period), sendMessage' (IncMasterN (-1))) -- %! Deincrement the number of windows in the master area
-
-    , subtitle "quit, or restart"
-    , ((modm .|. shiftMask, xK_q     ), addName "Quit" $ io exitSuccess) -- %! Quit xmonad
-    , ((modm              , xK_q     ), addName "Restart" $ spawn "xmonad --recompile && xmonad --restart") -- %! Restart xmonad
-    ]
+myDefaultKeys conf@XConfig {XMonad.modMask = modm} =
+  [ subtitle "launching and killing programs",
+    ((modm .|. shiftMask, xK_c), addName "Close the focused window" kill), -- %! Close the focused window
+    subtitle "changing layouts",
+    ((modm, xK_space), sendMessage' NextLayout), -- %! Rotate through the available layout algorithms
+    ((modm .|. shiftMask, xK_space), addName "Reset the layout" $ setLayout $ XMonad.layoutHook conf), -- %!  Reset the layouts on the current workspace to default
+    separator,
+    ((modm, xK_n), addName "Refresh" refresh), -- %! Resize viewed windows to the correct size
+    subtitle "move focus up or down the window stack",
+    ((modm, xK_j), addName "Focus down" $ windows W.focusDown), -- %! Move focus to the next window
+    ((modm, xK_k), addName "Focus up" $ windows W.focusUp), -- %! Move focus to the previous window
+    ((modm, xK_m), addName "Focus the master" $ windows W.focusMaster), -- %! Move focus to the master window
+    subtitle "modifying the window order",
+    ((modm, xK_Return), addName "Swap with the master" $ windows W.swapMaster), -- %! Swap the focused window and the master window
+    ((modm .|. shiftMask, xK_j), addName "Swap down" $ windows W.swapDown), -- %! Swap the focused window with the next window
+    ((modm .|. shiftMask, xK_k), addName "Swap up" $ windows W.swapUp), -- %! Swap the focused window with the previous window
+    subtitle "resizing the master/slave ratio",
+    ((modm, xK_h), sendMessage' Shrink), -- %! Shrink the master area
+    ((modm, xK_l), sendMessage' Expand), -- %! Expand the master area
+    subtitle "change the number of windows in the master area",
+    ((modm, xK_comma), sendMessage' (IncMasterN 1)), -- %! Increment the number of windows in the master area
+    ((modm, xK_period), sendMessage' (IncMasterN (-1))), -- %! Deincrement the number of windows in the master area
+    subtitle "quit, or restart",
+    ((modm .|. shiftMask, xK_q), addName "Quit" $ io exitSuccess), -- %! Quit xmonad
+    ((modm, xK_q), addName "Restart" $ spawn "xmonad --recompile && xmonad --restart") -- %! Restart xmonad
+  ]
