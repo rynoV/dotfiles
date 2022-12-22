df15707efc53a199874685374fbffd0aac6ca59c
Calum Sieppert <sieppertcalum@gmail.com>
2022-12-21 18:17:25 -0700

    Big updates to emacs configuration
    - Fix window splitting to split horizontally
    - Setup org-superstar and change org aesthetics
    - Remove yasnippet, replace with tempel
    - Remove company, replace with corfu, cape, and built-in completion
    - Allow for vertico-repeat and quick jumping in vertico
    - Improvements to embark config
    - Setup projectile other file for purescript
    - All the icons in more places
    - Replace evil with meow
    - Setup markdown and graphql
    - Setup avy for jumping around
    - Setup git links
    - Setup imenu-list sidebar
    - Change yanking to not use system clipboard by default
    - Format latex on save with latexindent
    - Keybind changes
    - Fix browse-url not working with direnv
    - Configure dabbrev
    - Fix compilation mode colours
    - Nord theme customizations
    - Setup code-review

--- private_dot_config/emacs/init.el
+++ private_dot_config/emacs/init.el
@@ -16,6 +16,22 @@
 (eval-and-compile
   (setq use-package-always-ensure t))
 
+;; Quelpa for installing from source/git repos
+;; https://github.com/quelpa/quelpa
+(unless (package-installed-p 'quelpa)
+  (with-temp-buffer
+    (url-insert-file-contents "https://raw.githubusercontent.com/quelpa/quelpa/master/quelpa.el")
+    (eval-buffer)
+    (quelpa-self-upgrade)))
+;; Install quelpa use-package integration
+;; https://github.com/quelpa/quelpa-use-package
+(unless (package-installed-p 'quelpa)
+  (quelpa
+   '(quelpa-use-package
+     :fetcher git
+     :url "https://github.com/quelpa/quelpa-use-package.git")))
+(require 'quelpa-use-package)
+
 ;; Load main config file "./config.org"
 (require 'org)
 (org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org")))
@@ -29,6 +45,8 @@
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
  '(browse-url-browser-function 'browse-url-default-browser)
+ '(custom-safe-themes
+   '("1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" default))
  '(lsp-ui-sideline-delay 0.5)
  '(lsp-ui-sideline-show-hover t)
  '(lsp-ui-sideline-update-mode nil)
@@ -40,9 +58,12 @@
                               (lambda nil
                                 (setq-local buffer-auto-save-file-name nil))))
  '(package-selected-packages
-   '(magit-delta git-link emacs-open-github-from-here evil-ledger flycheck-ledger ledger-mode git-gutter dhall-mode all-the-icons slack dirvish typescript-mode typescript ranger consult-lsp affe consult-company consult-yasnippet consult-dir consult-projectile embark-consult embark marginalia orderless vertico consult docker lsp-purescript purescript-mode minimap treemacs-projectile ripgrep rg projectile crm-custom ido-ubiquitous unicode-fonts nix-mode direnv org-crypt org-journal sudo-edit chezmoi-company chezmoi org flex-mode flex ligature yaml-mode lsp-treemacs flycheck evil-iedit-state lsp-ui lsp-ido lsp-haskell lsp-mode prolog-mode writeroom-mode zen-mode ox-latex org-super-agenda haskell-mode org-download org-contrib ox-extra org-web-tools ox-md evil-numbers magit-todos evil-easymotion lispyville evil-exchange evil-args evil-textobj-line forge general company-posframe yasnippet auctex texmathp cdlatex rich-minority quelpa-use-package org-sort-tasks org-roam undo-tree org-indent visual-line org-sidebar iflipb restart-emacs poet-theme doom-themes highlight-escape-sequences highlight-numbers evil-surround which-key diminish org-bullets use-package))
+   '(adaptive-wrap org-superstar org-inlinetask consult-flycheck tempel-collection tempel emacs-lisp kind-icon cape corfu all-the-icons-completion code-review graphql-mode ox-json imenu-list markdown hercules quelpa emacs-surround meow magit-delta git-link emacs-open-github-from-here flycheck-ledger ledger-mode git-gutter dhall-mode all-the-icons slack dirvish typescript-mode typescript ranger consult-lsp affe consult-dir consult-projectile embark-consult embark marginalia orderless vertico consult docker lsp-purescript purescript-mode minimap treemacs-projectile ripgrep rg projectile crm-custom ido-ubiquitous unicode-fonts nix-mode direnv org-crypt org-journal sudo-edit chezmoi-company chezmoi org flex-mode flex ligature yaml-mode lsp-treemacs flycheck lsp-ui lsp-ido lsp-haskell lsp-mode prolog-mode writeroom-mode zen-mode ox-latex org-super-agenda haskell-mode org-download org-contrib ox-extra org-web-tools ox-md magit-todos forge auctex texmathp cdlatex rich-minority quelpa-use-package org-sort-tasks org-roam undo-tree org-indent visual-line org-sidebar iflipb restart-emacs poet-theme doom-themes highlight-escape-sequences highlight-numbers which-key diminish use-package))
  '(safe-local-variable-values
-   '((lsp-haskell-formatting-provider . "fourmolu")
+   '((minor-mode . outline)
+     (lsp-haskell-server-path . "haskell-language-server-wrapper")
+     (lsp-haskell-server-path . "haskell-language-server-8.8.4")
+     (lsp-haskell-formatting-provider . "fourmolu")
      (eval defun org-babel-execute:yaml
            (body params)
            body)))
@@ -52,8 +73,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-tooltip ((t (:family calum/code-font))))
- '(company-tooltip-selection ((t (:background "dim gray"))))
+ '(hi-blue ((t (:background "#81A1C1" :foreground "black"))))
+ '(hi-green ((t (:background "#A3BE8C" :foreground "black"))))
+ '(hi-pink ((t (:background "#B48EAD" :foreground "black"))))
+ '(hi-yellow ((t (:background "#EBCB8B" :foreground "black"))))
  '(lsp-face-highlight-read ((t (:background "#363c4a" :foreground "#F0F4FC" :weight bold))))
  '(lsp-ui-sideline-symbol-info ((t (:extend t :background "#2E3440" :foreground "#656c7c"))))
+ '(meow-region-cursor-1 ((t (:background "#6eee88c8a463" :foreground "#ECEFF4"))))
+ '(meow-region-cursor-2 ((t (:background "#5c5c6fef8706" :foreground "#ECEFF4"))))
+ '(meow-region-cursor-3 ((t (:background "#49c9571669a9" :foreground "#ECEFF4"))))
+ '(org-block ((t (:extend t :background "#00000000"))))
+ '(org-block-begin-line ((t (:inherit org-block :extend t :background "#373E4C" :foreground "#6f7787"))))
+ '(org-hide ((t nil)))
+ '(region ((t (:extend t :background "#373e4c"))))
+ '(secondary-selection ((t (:extend t :background "#373e4c"))))
  '(show-paren-match ((t (:background "dim gray" :foreground "white")))))
+(put 'scroll-left 'disabled nil)
