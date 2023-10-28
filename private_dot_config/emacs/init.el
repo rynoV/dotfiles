e42223627a78e1352594f53a0c0dc46624393ff2
Calum Sieppert <sieppertcalum@gmail.com>
2023-10-28 04:44:50 -0600

    Switch to use straight.el, pdf-tools, org-noter

--- private_dot_config/emacs/init.el
+++ private_dot_config/emacs/init.el
@@ -1,38 +1,20 @@
-;;; Based off https://github.com/ianpan870102/yay-evil-emacs/blob/master/init.el
-(require 'package)
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
-;; For latest version of eglot
-(add-to-list 'package-archives '("gnu-devel" . "https://elpa.gnu.org/devel/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-;; This is needed for org-plus-contrib which provides ox-extra
-;; https://emacs.stackexchange.com/questions/8182/how-to-use-org-plus-contrib
-;; https://emacs.stackexchange.com/questions/38184/org-mode-ignore-heading-when-exporting-to-latex/41685#41685
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(setq package-enable-at-startup nil)
-(package-initialize)
+(setq straight-use-package-by-default t)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; Setting up the package manager. Install if missing.
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-and-compile
-  (setq use-package-always-ensure t))
-
-;; Quelpa for installing from source/git repos
-;; https://github.com/quelpa/quelpa
-(unless (package-installed-p 'quelpa)
-  (with-temp-buffer
-    (url-insert-file-contents "https://raw.githubusercontent.com/quelpa/quelpa/master/quelpa.el")
-    (eval-buffer)
-    (quelpa-self-upgrade)))
-;; Install quelpa use-package integration
-;; https://github.com/quelpa/quelpa-use-package
-(unless (package-installed-p 'quelpa)
-  (quelpa
-   '(quelpa-use-package
-     :fetcher git
-     :url "https://github.com/quelpa/quelpa-use-package.git")))
-(require 'quelpa-use-package)
+(straight-use-package 'use-package)
+(straight-use-package 'org)
+(setq use-package-always-demand t)
 
 ;; Need this as it is not loaded early enough with the daemon for some
 ;; reason, gave error "Eager macro-expansion failure: (void-function
@@ -41,7 +23,6 @@
 (require 'cl)
 
 ;; Load main config file "./config.org"
-(require 'org)
 (org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org")))
 
 (provide 'init)
@@ -80,7 +61,7 @@
      ("" "capt-of" nil nil)
      ("colorlinks=true,linkcolor=darkgray,unicode=true,psdextra" "hyperref" nil nil)))
  '(package-selected-packages
-   '(org-roam-ui org-mind-map persp-mode-projectile-bridge bufler centaur-tabs web-mode eglot eldoc-box persp-mode eyebrowse spaceline spaceline-config autothemer os1-theme org-fragtog rust-mode elfeed goto-last-change smart-region topsy scroll-on-jump scrollkeeper aggressive-indent mosey hungry-delete dashboard smartparens goggles org-appear origami org-modern ox-ipynb pdf-tools json-navigator json-mode dart-mode dart ox-ravel ess diff-hl elisp-format visual-regexp multiple-cursors combobulate tree-sitter-langs tree-sitter delight ox-gfm general consult-flymake consult-eglot adaptive-wrap org-superstar org-inlinetask tempel-collection tempel emacs-lisp kind-icon cape corfu all-the-icons-completion code-review graphql-mode ox-json imenu-list markdown hercules quelpa emacs-surround meow magit-delta git-link emacs-open-github-from-here ledger-mode git-gutter dhall-mode all-the-icons slack dirvish typescript-mode typescript ranger affe consult-dir consult-projectile embark-consult embark marginalia orderless vertico consult docker lsp-purescript purescript-mode minimap treemacs-projectile ripgrep rg projectile crm-custom ido-ubiquitous unicode-fonts nix-mode direnv org-crypt org-journal sudo-edit chezmoi-company chezmoi org flex-mode flex ligature yaml-mode lsp-ido prolog-mode writeroom-mode zen-mode ox-latex org-super-agenda haskell-mode org-download org-contrib ox-extra org-web-tools ox-md magit-todos forge auctex texmathp cdlatex rich-minority quelpa-use-package org-sort-tasks org-roam undo-tree org-indent visual-line org-sidebar iflipb restart-emacs poet-theme doom-themes highlight-escape-sequences highlight-numbers which-key diminish use-package))
+   '(image-roll org-noter org-roam-ui org-mind-map persp-mode-projectile-bridge bufler centaur-tabs web-mode eglot eldoc-box persp-mode eyebrowse spaceline spaceline-config autothemer os1-theme org-fragtog rust-mode elfeed goto-last-change smart-region topsy scroll-on-jump scrollkeeper aggressive-indent mosey hungry-delete dashboard smartparens goggles org-appear origami org-modern ox-ipynb json-navigator json-mode dart-mode dart ox-ravel ess diff-hl elisp-format visual-regexp multiple-cursors combobulate tree-sitter-langs tree-sitter delight ox-gfm general consult-flymake consult-eglot adaptive-wrap org-superstar org-inlinetask tempel-collection tempel emacs-lisp kind-icon cape corfu all-the-icons-completion code-review graphql-mode ox-json imenu-list markdown hercules quelpa emacs-surround meow magit-delta git-link emacs-open-github-from-here ledger-mode git-gutter dhall-mode all-the-icons slack dirvish typescript-mode typescript ranger affe consult-dir consult-projectile embark-consult embark marginalia orderless vertico consult docker lsp-purescript purescript-mode minimap treemacs-projectile ripgrep rg projectile crm-custom ido-ubiquitous unicode-fonts nix-mode direnv org-crypt org-journal sudo-edit chezmoi-company chezmoi org flex-mode flex ligature yaml-mode lsp-ido prolog-mode writeroom-mode zen-mode ox-latex org-super-agenda haskell-mode org-download org-contrib ox-extra org-web-tools ox-md magit-todos forge auctex texmathp cdlatex rich-minority quelpa-use-package org-sort-tasks org-roam undo-tree org-indent visual-line org-sidebar iflipb restart-emacs poet-theme doom-themes highlight-escape-sequences highlight-numbers which-key diminish use-package))
  '(safe-local-variable-values
    '((org-export-initial-scope . subtree)
      (org-fragtog-mode)
